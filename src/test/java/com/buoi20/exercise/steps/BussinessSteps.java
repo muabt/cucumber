27a19467c2cf290a96e3f56c21fdd58f28ca85ef
muabt <hoamuacntt14@gmail.com>
2022-05-19 14:55:04 +0700

    Buổi 20: Cucumber exercise update

--- src/test/java/com/buoi20/exercise/steps/BussinessSteps.java
+++ src/test/java/com/buoi20/exercise/steps/BussinessSteps.java
@@ -5,6 +5,7 @@ import com.buoi20.exercise.Visit;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.eo.Do;
 import org.junit.jupiter.api.Assertions;
 
 import java.text.ParseException;
@@ -21,8 +22,8 @@ public class BussinessSteps {
 
     }
 
-    @When("khach hang name <name> la member <isMember> loai <memberType> ngay <date> su dung dich vu <serviceExpense> va san pham <productExpense>")
-    public void tinhPhi(String name, boolean isMember, String memberType, String date, String serviceExpense, String productExpense) throws ParseException {
+    @When("^khach hang name (.+) la member (.+) loai (.+) ngay (.+) su dung dich vu (.+) va san pham (.+)$")
+    public void tinhPhi(String name, boolean isMember, String memberType, String date, String serviceExpense, String productExpense)  throws Throwable{
         customer.setName(name);
         customer.setMember(isMember);
         customer.setMemberType(memberType);
@@ -35,16 +36,23 @@ public class BussinessSteps {
         if(productExpense != null){
             cProductExpense = Double.parseDouble(productExpense);
         }
+        if(serviceExpense == null){
+            cServiceExpense = 0;
+        }
+        if(productExpense == null){
+            cProductExpense = 0;
+        }
         visit.setServiceExpense(cServiceExpense);
         visit.setProductExpense(cProductExpense);
-        double actualTotalExpense = visit.getTotalExpense();
+        actualTotalExpense = visit.getTotalExpense();
     }
 
 
-    @Then("tinh tong chi phi cho khach hang la <totalExpense>")
-    public void tinhTongChiPhiChoKhachHangLa(double expectedTotalExpense) {
-        Assertions.assertEquals(expectedTotalExpense,actualTotalExpense);
+    @Then("^tinh tong chi phi cho khach hang la (.+)$")
+    public void tinhTongChiPhiChoKhachHangLa(double expectedTotalExpense)  throws Throwable{
+        Assertions.assertEquals(expectedTotalExpense,actualTotalExpense,0.0009);
 
 
     }
+
 }
--- src/test/resources/features/business.feature
+++ src/test/resources/features/business.feature
@@ -1,17 +1,18 @@
 Feature: thu nghiem tinh phi visit
+
   Scenario Outline: tinh tong chi phi visit
     Given tao khach hang <c>
     When khach hang name <name> la member <isMember> loai <memberType> ngay <date> su dung dich vu <serviceExpense> va san pham <productExpense>
     Then tinh tong chi phi cho khach hang la <totalExpense>
 
     Examples:
-    |name|isMember|memberType|date|serviceExpense|productExpense|totalExpense|
-    |John|TRUE|GOLD|1/1/2022|500|400|785                                     |
-    |Jame|FALSE||10/10/2021|499.99|349.99|849.98                             |
-    |Emily|TRUE|PREMIUM|5/10/2021|799|567.999|1150.399                       |
-    |Jessica|TRUE|PREMIUM|5/1/2022|799||639.2                                |
-    |Brian|FALSE||7/1/2022|899.99||899.99                                    |
-    |Hiddleston|TRUE|SILVER|10/10/2020|789|199|889.2                         |
+      | name       | isMember | memberType | date       | serviceExpense | productExpense | totalExpense |
+      | John       | TRUE     | GOLD       | 1/1/2022   | 500.0          | 400.0          | 785.0        |
+      | Jame       | FALSE    |            | 10/10/2021 | 499.99         | 349.99         | 849.98       |
+      | Emily      | TRUE     | PREMIUM    | 5/10/2021  | 799.0          | 567.999        | 1150.399     |
+      | Jessica    | TRUE     | PREMIUM    | 5/1/2022   | 799.0          |                | 639.2        |
+      | Brian      | FALSE    |            | 7/1/2022   | 899.99         |                | 899.99       |
+      | Hiddleston | TRUE     | SILVER     | 10/10/2020 | 789.0          | 199.0          | 889.2        |
 
 
 
